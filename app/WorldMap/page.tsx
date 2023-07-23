e2c0aac6ff5a7c2462882b15d7f7186bf6a4135b
Daanish <daanishkhazi@Daanishs-MacBook-Air.local>
2023-07-23 00:25:32 -0700

    fixed coordinates

--- app/WorldMap/page.tsx
+++ app/WorldMap/page.tsx
@@ -4,45 +4,42 @@ import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 
 export default function WorldMap() {
-  const divRef = useRef();
+  const divRef = useRef<HTMLDivElement | null>(null);
   const [coordinates, setCoordinates] = useState({ lon: 0, lat: 0 });
 
   useEffect(() => {
     d3.xml(
-      "https://upload.wikimedia.org/wikipedia/commons/9/9f/BlankMap-World-Equirectangular.svg"
+      "https://upload.wikimedia.org/wikipedia/commons/5/51/BlankMap-Equirectangular.svg"
     ).then((data) => {
       const divNode = divRef.current;
+      if (!divNode) return;
       divNode.innerHTML = "";
       divNode.appendChild(data.documentElement);
 
       const svg = d3.select(divNode).select("svg");
       const svgWidth = divNode.getBoundingClientRect().width;
       const svgHeight = divNode.getBoundingClientRect().height;
+      svg.attr("width", svgWidth * 2).attr("height", svgHeight * 2);
 
       svg.on("mousemove", (event) => {
-        // Get the mouse coordinates
         const [x, y] = d3.pointer(event);
-
-        // Calculate the longitude and latitude
         const lon = (x / svgWidth) * 360 - 180;
         const lat = 90 - (y / svgHeight) * 180;
         setCoordinates({ lon, lat });
 
-        // Create a circle at the mouse position
         const circle = svg
           .append("circle")
           .attr("cx", x)
           .attr("cy", y)
           .attr("r", 1)
           .style("fill", "none")
-          .style("stroke", "red")
+          .style("stroke", "#FCFDF7")
           .style("stroke-opacity", 1);
 
-        // Animate the circle to grow and fade out
         circle
           .transition()
-          .duration(1000)
-          .attr("r", 50)
+          .duration(2000)
+          .attr("r", 20)
           .style("stroke-opacity", 0)
           .remove();
       });
@@ -50,11 +47,11 @@ export default function WorldMap() {
   }, []);
 
   return (
-    <div>
-      <div ref={divRef} style={{ width: "100%", height: "auto" }} />
+    <div className="flex-col py-36">
+      <div ref={divRef} className="w-full h-auto" />
       <p>
-        Longitude: {coordinates.lon.toFixed(2)}, Latitude:{" "}
-        {coordinates.lat.toFixed(2)}
+        Latitude: {coordinates.lat.toFixed(2)}, Longitude:{" "}
+        {coordinates.lon.toFixed(2)}
       </p>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
+import { AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
+import Link from "next/link";
 import WorldMap from "./WorldMap/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex bg-[#FCFDF7] min-h-screen flex-col items-center p-24">
+      <header className="w-full max-w-2xl py-8 text-3xl  text-center text-gray-900">
+        <h1 className="text-6xl font-bold">BourdainGPT</h1>
+        <p className="mt-2 text-lg text-gray-900">
+          Explore the world&#39;s culinary traditions
+        </p>
+      </header>
+
       <WorldMap />
+
+      <>
+        <p className="italic">
+          Note: Using the best open source equirectangular projection I could
+          find, please note that several borders may not be accurate{" "}
+        </p>
+        <Link
+          href="https://github.com/daanishkhazi/bourdain_gpt"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillGithub className="text-4xl mt-4 hover:scale-110 transition ease-in-out delay-50" />
+        </Link>
+      </>
     </main>
   );
 }
